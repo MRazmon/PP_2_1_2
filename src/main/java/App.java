9451bf7061b6a9b35f5c0c8dc24a37ed7417d0b9
MRazmon <razmon.mail@gmail.com>
2026-05-26 20:58:50 +0300

    Initial commit: Spring HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean beanEqual = (bean1 == bean2);
+        System.out.println(beanEqual);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean catEqual = (cat1 == cat2);
+        System.out.println(catEqual);
     }
 }
